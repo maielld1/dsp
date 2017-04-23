bf33d8b21c13b1bf5493b7ffff573813b998cec5
Devin Maiello <maielld1@tcnj.edu>
2017-04-23 12:20:14 -0400

    q5 complete

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,29 @@
 # Hint:  use Google to find python function
+from datetime import datetime
 
 ####a) 
 date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+date_stop = '07-28-2015'
+
+start_a = datetime.strptime(date_start, '%m-%d-%Y')
+stop_a = datetime.strptime(date_stop, '%m-%d-%Y')
+
+print stop_a - start_a
 
 ####b)  
 date_start = '12312013'  
-date_stop = '05282015'  
+date_stop = '05282015'
+
+start_b = datetime.strptime(date_start, '%m%d%Y')
+stop_b  = datetime.strptime(date_stop, '%m%d%Y')
+
+print stop_b - start_b
 
 ####c)  
 date_start = '15-Jan-1994'  
 date_stop = '14-Jul-2015'  
+
+start_c = datetime.strptime(date_start, '%d-%b-%Y')
+stop_c = datetime.strptime(date_stop, '%d-%b-%Y')
+
+print stop_c - start_c
