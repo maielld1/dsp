76eb45ebd145776ff7a2fbda73ffccc2c6d65069
Devin Maiello <maielld1@tcnj.edu>
2017-04-23 17:29:13 -0400

    Part 2 Complete

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,15 @@
-PLACE YOUR CODE HERE
+import csv
+
+f = open("faculty.csv", "r")
+
+faculty = list(csv.reader(f))
+data = faculty[1:]
+
+emails = []
+for row in data:
+    emails.append(row[3].strip())
+    
+with open("emails.csv", "w") as f:
+    writer = csv.writer(f, lineterminator = '\n')
+    for email in emails:
+        writer.writerow([email])
